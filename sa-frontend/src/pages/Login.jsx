eb76a158f0ea56d09e34dec1ac8f2884e1af2dc2
Moses Onche <onchemoses38@gmail.com>
2024-01-08 16:00:09 +0100

    Add background image to home page

--- sa-frontend/src/pages/Login.jsx
+++ sa-frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="bg-[url('/src/assets/5172658.jpg')] h-screen w-full flex justify-center items-center text-3xl text-white">
       <div className='w-2/5 h-4/5 bg-slate-100 rounded-lg text'>
