653635474469bd8612c55598de3a939d609c359b
Moses Onche <onchemoses38@gmail.com>
2024-01-08 15:28:51 +0100

    Update home page style

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -2,9 +2,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
     <Home />
     </div>
   );
--- sa-frontend/src/pages/Home.jsx
+++ sa-frontend/src/pages/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center text-3xl bg-gray-700 text-white'>Home</div>
+    <div className='h-screen w-full flex justify-center items-center text-3xl bg-gray-700 text-white'>Home</div>
   )
 }
